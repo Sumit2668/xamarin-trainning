05c366e739ecb721d3777315bdcecca775382199
Le Hoai <lehoai.214@gmail.com>
2015-01-10 16:43:28 +0700

    Create HttpClientUtils

---
+++ G04/JiraMobile/JiraMobile/JiraMobile/Utils/HttpClientUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraMobile.Utils
+{
+    public class HttpClientUtils
+    {
+        public static string getGoogle()
+        {
+            HttpClient client = new HttpClient();
+            return null;
+        }
+    }
+}
