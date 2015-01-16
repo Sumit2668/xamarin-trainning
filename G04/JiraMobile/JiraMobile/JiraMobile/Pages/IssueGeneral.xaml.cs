f0ae19b51680665e58d10a52666601dcc3afc1a8
Le Hoai <lehoai.214@gmail.com>
2015-01-16 22:03:22 +0700

    HoaiLV add CallBack display process bar async

--- G04/JiraMobile/JiraMobile/JiraMobile/Pages/IssueGeneral.xaml.cs
+++ G04/JiraMobile/JiraMobile/JiraMobile/Pages/IssueGeneral.xaml.cs
@@ -4,12 +4,24 @@ using Xamarin.Forms;
 
 namespace JiraMobile.Pages
 {	
-	public partial class IssueGeneral : ContentPage
-	{	
+	public partial class IssueGeneral : ContentPage, JiraMobile.Pages.HttpClientUtils.IProcessBarCallBack
+	{
 		public IssueGeneral ()
 		{
 			InitializeComponent ();
-			//lbTest.Text = new HttpClientUtils ().getAllIssues ();
+			lbTest.Text = new HttpClientUtils ().getAllIssues ();
+		}
+
+		public void Show()
+		{
+			processBar.IsVisible = true;
+			viewContent.IsVisible = false;
+		}
+
+		public void Hide()
+		{
+			processBar.IsVisible = false;
+			viewContent.IsVisible = true;
 		}
 	}
 }
--- G04/JiraMobile/JiraMobile/JiraMobile/Utils/HttpClientUtils.cs
+++ G04/JiraMobile/JiraMobile/JiraMobile/Utils/HttpClientUtils.cs
@@ -4,46 +4,65 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace JiraMobile.Pages
 {
     public class HttpClientUtils
     {
+		// TODO : Mock data from Login screen
 		private string username = "HoaiLV2";
 		private string password = "XXXXXXX";
+		private IProcessBarCallBack _IProcessBarCallBack;
 
 		private string authStrBuild = "";
 
 		private string baseURL = "https://insight.fsoft.com.vn/jira/";
 
+		// TODO : Constructor Mock
 		public HttpClientUtils()
 		{
 			authStrBuild = System.Convert.ToBase64String (Encoding.UTF8.GetBytes (username + ":" + password));
 		}
 
-		public string getAllIssues()
+		public HttpClientUtils(string username, string password, IProcessBarCallBack _IProcessBarCallBack)
 		{
-			return executeApi ("rest/api/2/search?jql=assignee=hoailv2");
+			this.username = username;
+			this.password = password;
+			this._IProcessBarCallBack = _IProcessBarCallBack;
 		}
 
-		public string executeApi(string url)
+		public string getAllIssues()
 		{
+			return executeApi ("rest/api/2/search?jql=assignee=hoailv2").ToString();
+		}
+
+		public async Task<string> executeApi(string url)
+		{
+			// display processbar
+			this._IProcessBarCallBack.Show ();
+
 			var request = new HttpRequestMessage () {
 
 				RequestUri = new Uri(baseURL + url),
 				Method = HttpMethod.Get
 			};
 
-			HttpClient client = new HttpClient ();
-			client.DefaultRequestHeaders.Add ("Authorization", "Basic " + authStrBuild);
-			client.DefaultRequestHeaders.Add ("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0");
 			request.Headers.Add ("Authorization", "Basic " + authStrBuild);
 			request.Headers.Add ("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0");
 
-			HttpClient client2 = new HttpClient ();
-			var response = client2.SendAsync (request).Result.Content.ReadAsStringAsync().Result;
+			HttpClient client = new HttpClient ();
+			var response = await client.SendAsync (request).Result.Content.ReadAsStringAsync ();
 
-			return client.GetStringAsync (baseURL + url).Result;
+			this._IProcessBarCallBack.Hide ();
+
+			return response;
+		}
+
+		public interface IProcessBarCallBack
+		{
+			void Show ();
+			void Hide();
 		}
     }
 }
