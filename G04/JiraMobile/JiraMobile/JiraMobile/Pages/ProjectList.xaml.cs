c880150c48e68e045e5bb69a75d423ec422c48cb
Le Hoai <lehoai.214@gmail.com>
2015-02-02 22:52:04 +0700

    Add Project List page

---
+++ G04/JiraMobile/JiraMobile/JiraMobile/Pages/ProjectList.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace JiraMobile
+{	
+	public partial class ProjectList : ContentPage
+	{	
+		public ProjectList ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
