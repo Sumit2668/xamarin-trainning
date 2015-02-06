29cf6d3249253ea1f018ee5cf4bb2977c8c58cc2
Le Hoai <lehoai.214@gmail.com>
2015-02-06 17:11:50 +0700

    Change sqlite to sharepreference

--- G04/JiraMobile/JiraMobile/JiraMobile.Android/Utils/UserData.cs
+++ G04/JiraMobile/JiraMobile/JiraMobile.Android/Utils/UserData.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using JiraMobile.Utils;
-using Xamarin.Forms;
 using SQLite;
+using Android.App;
+using Android.Content;
 
-[assembly: Dependency (typeof(JiraMobile.Droid.UserData))]
+[assembly: Xamarin.Forms.Dependency (typeof(JiraMobile.Droid.UserData))]
 namespace JiraMobile.Droid
 {
 	public class UserData : IUserData
@@ -16,21 +17,29 @@ namespace JiraMobile.Droid
 
 		public void saveAuth (string token)
 		{
-			string folder = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
-			var conn = new SQLiteConnection (System.IO.Path.Combine (folder, "jira.db"));
-			conn.CreateTable<UserLogin> ();
-			conn.DeleteAll<UserLogin> ();
-			var data = new UserLogin {
-				Token = token
-			};
-			conn.Insert (data);
+//			string folder = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+//			var conn = new SQLiteConnection (System.IO.Path.Combine (folder, "jira.db"));
+//			conn.CreateTable<UserLogin> ();
+//			conn.DeleteAll<UserLogin> ();
+//			var data = new UserLogin {
+//				Token = token
+//			};
+//			conn.Insert (data);
+
+			var prefs = Application.Context.GetSharedPreferences("JiraApp", FileCreationMode.Private);
+			var prefsEditor = prefs.Edit();
+			prefsEditor.PutString("Login", token);
+			prefsEditor.Commit();
 		}
 
 		public string getAuth ()
 		{
-			string folder = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
-			var conn = new SQLiteConnection (System.IO.Path.Combine (folder, "jira.db"));
-			return conn.Table<	UserLogin> ().FirstOrDefault ().Token;
+//			string folder = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+//			var conn = new SQLiteConnection (System.IO.Path.Combine (folder, "jira.db"));
+//			return conn.Table<	UserLogin> ().FirstOrDefault ().Token;
+			var prefs = Application.Context.GetSharedPreferences("JiraApp", FileCreationMode.Private);
+			var value = prefs.GetString ("Login", null);
+			return value;
 		}
 
 		#endregion
